94e9fc8efc6f446a80bb7a267af030f1dbdb13cf
kim.biloni <kim.biloni@he-arc.ch>
2019-03-03 14:49:17 +0100

    add a home page

--- src/main/java/com/hearc/theweb/TheWebApplication.java
+++ src/main/java/com/hearc/theweb/TheWebApplication.java
@@ -1,10 +1,17 @@
-package com.hearc.biloni;
+package com.hearc.theweb;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TheWebApplication {
+
+	@PostConstruct
+	public void init() {
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheWebApplication.class, args);
---
+++ src/main/java/com/hearc/theweb/controller/homeController.java
@@ -0,0 +1,13 @@
+package com.hearc.theweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+
+	@GetMapping(value = "/")
+	public String getHome() {
+		return "home";
+	}
+}
