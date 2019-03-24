6455c5e8a14fcf3405f2d5403ec0130fe4d5e730
kim.biloni <kim.biloni@he-arc.ch>
2019-03-24 20:43:15 +0100

    add search template and basic flow + minor design fix

--- src/main/java/com/hearc/theweb/controller/homeController.java
+++ src/main/java/com/hearc/theweb/controller/homeController.java
@@ -1,13 +1,30 @@
 package com.hearc.theweb.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.hearc.theweb.models.entites.Card;
+import com.hearc.theweb.models.repositories.CardsRepository;
 
 @Controller
 public class homeController {
 
+	@Autowired
+	CardsRepository cardsRepository;
+
 	@GetMapping(value = "/")
-	public String getHome() {
-		return "home";
+	public String getHome(@RequestParam(value = "search", required = false) String q, Map<String, Object> model) {
+		if (q != null && !q.isEmpty()) {
+			Iterable<Card> cards = cardsRepository.findAll();
+			model.put("searched_value", q);
+			model.put("cards", cards);
+			return "search_results";
+		} else {
+			return "home";
+		}
 	}
 }
