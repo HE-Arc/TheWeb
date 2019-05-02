ffd04f0f0e6c3f2cb7cc78e15be7fdd1e29be73f
kim.biloni <kim.biloni@he-arc.ch>
2019-05-02 20:21:40 +0200

    changing test suit to execute with katalon

--- runTest.sh
+++ runTest.sh
@@ -4,4 +4,4 @@
 Xvfb :99 &
 export local_addr="127.0.0.1:8080"
 
-/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/TheWebTest/TheWebTest.prj" -retry=0 -testSuitePath="Test Suites/TS_Authentification" -executionProfile="default" -browserType="Firefox (headless)"
+/Katalon_Studio_Linux_64-5.7.1/katalon -noSplash  -runMode=console -projectPath="$(pwd)/TheWebTest/TheWebTest.prj" -retry=0 -testSuitePath="Test Suites/CLTS05-TS05" -executionProfile="default" -browserType="Firefox (headless)"
